36887d6dc214f7bf82cf7163a6a304ecf5b38d65
coaspe <aspalt85@gmail.com>
2024-01-23 17:23:21 +0900

    Initial commit

--- target/debug/deps/auto_spell_checker-0026b430c4cc3f25.d
+++ target/debug/deps/auto_spell_checker-0026b430c4cc3f25.d
@@ -1,6 +1,6 @@
-C:\Users\URam Lee\Documents\GitHub\Algorithm\hello\target\debug\deps\libauto_spell_checker-0026b430c4cc3f25.rmeta: src\main.rs src\tray.rs
+C:\Users\URam Lee\Documents\GitHub\Algorithm\Auto spell checker\target\debug\deps\libauto_spell_checker-0026b430c4cc3f25.rmeta: src\main.rs src\tray.rs
 
-C:\Users\URam Lee\Documents\GitHub\Algorithm\hello\target\debug\deps\auto_spell_checker-0026b430c4cc3f25.d: src\main.rs src\tray.rs
+C:\Users\URam Lee\Documents\GitHub\Algorithm\Auto spell checker\target\debug\deps\auto_spell_checker-0026b430c4cc3f25.d: src\main.rs src\tray.rs
 
 src\main.rs:
 src\tray.rs:
