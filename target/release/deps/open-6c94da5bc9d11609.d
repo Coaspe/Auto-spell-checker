689c852f3009ed300efa296bd947a6e22ab2dcaa
coaspe <aspalt85@gmail.com>
2024-01-24 09:22:18 +0900

    Init

---
+++ target/release/deps/open-6c94da5bc9d11609.d
@@ -0,0 +1,8 @@
+C:\Users\URam Lee\Documents\GitHub\Algorithm\Auto spell checker\target\release\deps\libopen-6c94da5bc9d11609.rmeta: C:\Users\URam\ Lee\.cargo\registry\src\index.crates.io-6f17d22bba15001f\open-5.0.1\src\lib.rs C:\Users\URam\ Lee\.cargo\registry\src\index.crates.io-6f17d22bba15001f\open-5.0.1\src\windows.rs
+
+C:\Users\URam Lee\Documents\GitHub\Algorithm\Auto spell checker\target\release\deps\libopen-6c94da5bc9d11609.rlib: C:\Users\URam\ Lee\.cargo\registry\src\index.crates.io-6f17d22bba15001f\open-5.0.1\src\lib.rs C:\Users\URam\ Lee\.cargo\registry\src\index.crates.io-6f17d22bba15001f\open-5.0.1\src\windows.rs
+
+C:\Users\URam Lee\Documents\GitHub\Algorithm\Auto spell checker\target\release\deps\open-6c94da5bc9d11609.d: C:\Users\URam\ Lee\.cargo\registry\src\index.crates.io-6f17d22bba15001f\open-5.0.1\src\lib.rs C:\Users\URam\ Lee\.cargo\registry\src\index.crates.io-6f17d22bba15001f\open-5.0.1\src\windows.rs
+
+C:\Users\URam\ Lee\.cargo\registry\src\index.crates.io-6f17d22bba15001f\open-5.0.1\src\lib.rs:
+C:\Users\URam\ Lee\.cargo\registry\src\index.crates.io-6f17d22bba15001f\open-5.0.1\src\windows.rs:
