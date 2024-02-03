df994f7559bdc6264a85e561acd09adc3ac356b3
Coaspe <aspalt85@gmail.com>
2024-02-03 13:38:53 +0900

    feat: Kill running executor

--- Windows/Auto-spell-checker-patcher/src/main.rs
+++ Windows/Auto-spell-checker-patcher/src/main.rs
@@ -1,17 +1,31 @@
 use ascu::Downloader;
 use std::env::args;
 use std::process::Command;
+use sysinfo::{System, SystemExt};
 
 #[tokio::main]
 async fn main() {
     let args: Vec<String> = args().collect();
 
-    if args.len() < 2 {
+    if args.len() < 3 {
         return;
     };
 
-    let downloader = Downloader::new();
     let path: &str = &(args[1]);
+    let pid = args[2].parse::<u32>().unwrap();
+
+    // Kill EXECUTOR_EXE if it's running
+    let mut system = System::new_all();
+    system.refresh_all();
+
+    for (p, process) in system.get_processes() {
+        if p == pid {
+            let _ = process.kill(sysinfo::Signal::Kill);
+            break;
+        }
+    }
+
+    let downloader = Downloader::new();
     let result = downloader.download_executor(path).await;
 
     match result {
